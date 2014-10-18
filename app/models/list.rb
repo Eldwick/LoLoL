564d78aabdd518c7dd9e26acde479699d20ca418
flemdizzle <fleminsh@gmail.com>
2014-10-18 17:29:15 -0400

    got the relationship parser working for lol and list

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,11 +4,29 @@ class List < ActiveRecord::Base
     scope :random, -> {offset(rand(List.count)).first}
     scope :search, -> (search) {where("lower(title) like ?", "%#{search.downcase}%").uniq}
 
-    def load
-      ListOfList.all.each do |element|
-          element.each do |entry|
-            List.create( text: entry[0] , list_id: entry[1] )
-          end
+    def self.wiki(title)
+      Wikipedia.find(title)
+    end
+
+    def self.load
+      List.destroy_all
+      ListOfList.all.each do |list|
+         if List.wiki(list.title).content == nil
+            list.delete
+         else
+            output = List.wiki(list.title).content.scan(/\[\[(.*?)\]\]/).flatten
+            output.each do |title|
+               List.create(title: title, listoflist_id: list.id) if title.length < 40
+            end
+         end
       end
     end
+
+    def self.scrub
+      List.all.each do |list|
+         if List.wiki(list.title).content == nil
+            list.delete
+         end
+      end
+   end
 end
--- app/models/list_of_list.rb
+++ app/models/list_of_list.rb
@@ -10,5 +10,5 @@ class ListOfList < ActiveRecord::Base
          ListOfList.create!(title: list)
       end
    end
-   
+ 
 end
