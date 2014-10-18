7a1a0e36ad48bde2c9c301dc62c4287c0af6c742
flemdizzle <fleminsh@gmail.com>
2014-10-18 03:39:02 -0400

    finished the method to load contents to the lol DB

--- app/models/list_of_list.rb
+++ app/models/list_of_list.rb
@@ -1,13 +1,14 @@
 class ListOfList < ActiveRecord::Base
-
-   def load
-      page = Wikipedia.find('List of lists of lists')
+# run this from the rails console once and only once
+# to populate the DB
+   def self.load
       page = Wikipedia.find('List of lists of lists')
       raw = page.content
       var = raw.scan(/\[(.*?)\]/)
       output = var.join(",").gsub!(/\[/, '').split(",")
       output.each do |list|
-         ListOfList.create!(name: list)
+         ListOfList.create!(title: list)
       end
    end
+   
 end
