a7ead29748699131d57bfe4155d4ced64d3a42b5
flemdizzle <fleminsh@gmail.com>
2014-10-18 13:04:35 -0400

    wrote load script for List

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,14 @@
 class List < ActiveRecord::Base
-   has_many :entries
-   scope :random, -> {offset(rand(List.count)).first}
-   scope :search, -> (search) {where("lower(title) like ?", "%#{search.downcase}%").uniq}
+    has_many :entries
+    belongs_to :listoflist
+    scope :random, -> {offset(rand(List.count)).first}
+    scope :search, -> (search) {where("lower(title) like ?", "%#{search.downcase}%").uniq}
 
+    def load
+      ListOfList.all.each do |element|
+          element.each do |entry|
+            List.create( text: entry[0] , list_id: entry[1] )
+          end
+      end
+    end
 end
