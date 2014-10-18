946ee21b36c742dd24c4dab368ff0c274634befa
Eldwick <jamesjalandoni1@gmail.com>
2014-10-17 23:22:17 -0400

    Added game logic
    Instance variables score, seconds_allocated, correct_entries_ids. And functions check and calculate time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def show
+    @entries = Entry.where(list_id: params[:list_id])
+    @game = Game.new(@entries)
   end
 
-  def create
-  end
 end
+
+
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,34 @@
 class Game < ActiveRecord::Base
+  def initialize(entries)
+    @score = 0
+    @seconds_allocated = calculate_time(entries.count)
+    @correct_entries_ids = []
+  end
+
+  def check
+    entry = @entries.find_by(text:params[:answer]).where.not(id: @correct_entries_ids)
+    if entry
+      @correct_entries_ids << entry.id
+      entry
+    else
+      false
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def calculate_time(num_of_entries)
+    seconds = num_of_entries * 5
+    if seconds < 30
+      return 30
+    elsif seconds > 300
+      return 300
+    else
+      seconds
+    end
+  end
+
 end
