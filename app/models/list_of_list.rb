c6a8a54f333182c0f53a14cf0b4d8b7c9ef45d72
flemdizzle <fleminsh@gmail.com>
2014-10-18 12:43:28 -0400

    refactored the ListOfList load method

--- app/models/list_of_list.rb
+++ app/models/list_of_list.rb
@@ -2,10 +2,9 @@ class ListOfList < ActiveRecord::Base
 # run this from the rails console once and only once
 # to populate the DB
    def self.load
+      ListOfList.delete_all
       page = Wikipedia.find('List of lists of lists')
-      raw = page.content
-      var = raw.scan(/\[(.*?)\]/)
-      output = var.join(",").gsub!(/\[/, '').split(",")
+      output = page.content.scan(/\[\[(.*?)\]\]/).flatten
       output.each do |list|
          ListOfList.create!(title: list)
       end
