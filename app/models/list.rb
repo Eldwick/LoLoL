b897ec1d3b7a1eddcadc36f0e1db76d241335f72
flemdizzle <fleminsh@gmail.com>
2014-10-19 13:59:24 -0400

    fixed the seed file

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,10 +7,23 @@ class List < ActiveRecord::Base
 
   def self.nuke(text)
     List.all.each do |list|
-      if list.title.downcase.include? "#{text}"
+      if list.title.include? "#{text}"
         list.delete
       end
     end
   end
+
+    def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|model| [model.name,model.year,model.trim,model.make_id] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
+
 
 end
