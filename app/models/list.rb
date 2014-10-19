94e526b2e1b05657b905ce431d0337cd5caa0516
flemdizzle <fleminsh@gmail.com>
2014-10-19 12:37:33 -0400

    cleaned up all of my load scripts and making a pull request for the seeds

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,35 +5,6 @@ class List < ActiveRecord::Base
   scope :random, -> {offset(rand(List.count)).first}
   scope :search, -> (search) {where("lower(title) like ?", "%#{search.downcase}%").uniq}
 
-  def self.wiki(title)
-    Wikipedia.find(title)
-  end
-
-  def self.load
-    ListOfList.all.each do |list|
-      output = List.wiki(list.title).content.scan(/\[\[(.*?)\]\]/).flatten
-      output.each do |title|
-        List.create(title: title, listoflist_id: list.id, game: false) if title.length < 40
-      end
-    end
-  end
-
-  def self.list_of_scrub
-    List.all.each do |list|
-      unless list.title.downcase.include? "list of"
-        list.delete
-      else
-        puts list.id
-      end
-    end
-  end
-
-  def self.valid_link_scrub
-    List.all.each do |list|
-      List.wiki(list.title).links == nil ? list.delete : puts(list.id)
-    end
-  end
-
   def self.nuke(text)
     List.all.each do |list|
       if list.title.downcase.include? "#{text}"
--- app/models/list_of_list.rb
+++ app/models/list_of_list.rb
@@ -1,14 +1,4 @@
 class ListOfList < ActiveRecord::Base
    has_many :lists
-# run this from the rails console once and only once
-# to populate the DB
-   def self.load
-      ListOfList.delete_all
-      page = Wikipedia.find('List of lists of lists')
-      output = page.content.scan(/\[\[(.*?)\]\]/).flatten
-      output.each do |list|
-         ListOfList.create!(title: list)
-      end
-   end
- 
+
 end
