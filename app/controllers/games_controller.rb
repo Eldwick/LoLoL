857aa43374deadb9d579465f470bb0dbfb0c4161
Eldwick <jamesjalandoni1@gmail.com>
2014-10-19 18:51:19 -0400

    SO CLOSE

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,7 @@ respond_to :html, :js
   def show
     @list = List.find(params[:id])
     @entries = Entry.where(list_id: @list.p_id)
-    @game = Game.new(@entries)
+    @game = Game.new.setup(@entries)
     
   end
 
